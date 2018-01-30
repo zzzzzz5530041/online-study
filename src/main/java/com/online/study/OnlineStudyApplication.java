74aed63bc95cd131c4beb42b6c68ce132e1d1757
zhuyang <zhuyang_cd@keruyun.com>
2018-01-30 12:42:27 +0800

    initial

--- src/main/java/com/online/study/OnlineStudyApplication.java
+++ src/main/java/com/online/study/OnlineStudyApplication.java
@@ -2,11 +2,20 @@ package com.online.study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class OnlineStudyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OnlineStudyApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineStudyApplication.class, args);
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String home() {
+        return "hello world";
+    }
 }
