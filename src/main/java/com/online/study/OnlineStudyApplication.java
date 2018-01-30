5eea98c9522566148a12c7d506b138e8f5a83c3c
zhuyang <zhuyang_cd@keruyun.com>
2018-01-30 12:35:24 +0800

    Initial commit

---
+++ src/main/java/com/online/study/OnlineStudyApplication.java
@@ -0,0 +1,12 @@
+package com.online.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineStudyApplication.class, args);
+	}
+}
