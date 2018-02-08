000b1a1c472fa2a6ecd419b424c2bc9e5888f954
zhuyang <zhuyang_cd@keruyun.com>
2018-02-08 16:04:39 +0800

    .ignore

--- src/main/java/com/online/study/OnlineStudyApplication.java
+++ src/main/java/com/online/study/OnlineStudyApplication.java
@@ -2,12 +2,14 @@ package com.online.study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@ComponentScan("com")
 public class OnlineStudyApplication {
 
     public static void main(String[] args) {
