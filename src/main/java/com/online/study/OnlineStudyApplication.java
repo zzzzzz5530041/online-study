0614273d97bcb848eebd37e1ee09260c1a762931
zhuyang <zhuyang_cd@keruyun.com>
2018-02-08 18:02:01 +0800

    搭

--- src/main/java/com/online/study/OnlineStudyApplication.java
+++ src/main/java/com/online/study/OnlineStudyApplication.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.ImportResource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @RestController
