63cbada554397b69beca75358f6854f1011bc0b3
zhuyang <zhuyang_cd@keruyun.com>
2018-02-08 17:09:11 +0800

    搭

--- src/main/java/com/online/study/OnlineStudyApplication.java
+++ src/main/java/com/online/study/OnlineStudyApplication.java
@@ -3,6 +3,8 @@ package com.online.study;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 @ComponentScan("com")
+@Configuration
+@ImportResource(value = {"applicationContext-root.xml"})
 public class OnlineStudyApplication {
 
     public static void main(String[] args) {
