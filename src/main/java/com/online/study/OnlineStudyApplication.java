b1774690fcf5fd37c28baea694f22eb6c48ac284
zhuyang <zhuyang_cd@keruyun.com>
2018-02-11 14:32:15 +0800

    静态资源

--- src/main/java/com/online/study/OnlineStudyApplication.java
+++ src/main/java/com/online/study/OnlineStudyApplication.java
@@ -1,31 +1,39 @@
 package com.online.study;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude = {WebMvcAutoConfiguration.class})
-@RestController
 @ComponentScan("com")
 @Configuration
 @ImportResource(value = {"applicationContext-root.xml"})
 public class OnlineStudyApplication {
 
-
     public static void main(String[] args) {
+
         SpringApplication.run(OnlineStudyApplication.class, args);
     }
 
 //    @RequestMapping(value = "/", method = RequestMethod.GET)
 //    public String home() {
 //        return "hello world";
+//    }
+
+//    @Bean
+//    public ViewResolver viewResolver() {
+//        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+//        templateResolver.setTemplateMode("XHTML");
+//        templateResolver.setPrefix("templates/");
+//        templateResolver.setSuffix(".html");
+//
+//        SpringTemplateEngine engine = new SpringTemplateEngine();
+//        engine.setTemplateResolver(templateResolver);
+//
+//        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+//        viewResolver.setTemplateEngine(engine);
+//        return viewResolver;
 //    }
 }
