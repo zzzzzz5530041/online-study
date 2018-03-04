c705cb8e4285c9778c6fa893995130de766b316f
zzzzzz5530041 <1qazxsw2>
2018-03-04 14:04:08 +0800

    rename

--- src/main/java/com/online/edu/OnlineEduApplication.java
+++ src/main/java/com/online/edu/OnlineEduApplication.java
@@ -13,11 +13,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Configuration
 @ImportResource(value = {"applicationContext-root.xml"})
 @Controller
-public class OnlineStudyApplication {
+public class OnlineEduApplication {
 
     public static void main(String[] args) {
 
-        SpringApplication.run(OnlineStudyApplication.class, args);
+        SpringApplication.run(OnlineEduApplication.class, args);
     }
     @GetMapping(value = "/")
     public String index(){
